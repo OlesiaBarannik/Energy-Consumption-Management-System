422397c3ae807fd488e9eef05644d04bd7495ec4
OlesiaBarannik <olesyabarannik23@gmail.com>
2024-05-30 00:11:18 +0300

    added calculation of resource depletion rate, added tests

--- energy_source.py
+++ energy_source.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 
+class ResourceDepletionRateCalculateError(Exception):
+    pass
+
 
 class EnergySource:
     def __init__(self, **kwargs):
@@ -7,7 +10,8 @@ class EnergySource:
         self.validate()
 
     def energy_output(self):
-        return f"{self.__class__.__name__} AnnualEnergyOutput {round(self.calculate_result())}"
+        return (f"{self.__class__.__name__} AnnualEnergyOutput {round(self.calculate_annual_energy_output())} "
+                f"ResourceDepletionRate {round(self.calculate_resource_depletion_rate(), 2)}")
 
     def validate(self):
         invalid_keys = []
@@ -18,25 +22,49 @@ class EnergySource:
             raise TypeError(f"{', '.join(invalid_keys)} must be a number.")
 
     @abstractmethod
-    def calculate_result(self):
+    def calculate_annual_energy_output(self):
+        pass
+
+    @abstractmethod
+    def calculate_resource_depletion_rate(self):
         pass
 
 
 class SolarPanel(EnergySource):
-    def calculate_result(self, k=15):
+    def calculate_annual_energy_output(self, k=15):
         return self.area * self.efficiency * k
+
+    def calculate_resource_depletion_rate(self, k=100):
+        if self.efficiency <= 0:
+            raise ResourceDepletionRateCalculateError
+        return k / self.efficiency
 
 
 class WindTurbine(EnergySource):
-    def calculate_result(self, k=150):
+    def calculate_annual_energy_output(self, k=150):
         return self.height * self.windspeedaverage * k
+
+    def calculate_resource_depletion_rate(self, k=1000):
+        if self.height <= 0 or self.windspeedaverage <= 0:
+            raise ResourceDepletionRateCalculateError
+        return k / (self.height * self.windspeedaverage)
 
 
 class HydroPlant(EnergySource):
-    def calculate_result(self, k=12):
+    def calculate_annual_energy_output(self, k=12):
         return self.flowrate * self.drop * k
+
+    def calculate_resource_depletion_rate(self):
+        if self.drop <= 0:
+            raise ResourceDepletionRateCalculateError
+        return self.flowrate / self.drop
 
 
 class OffshoreWindTurbine(EnergySource):
-    def calculate_result(self, k=160):
+    def calculate_annual_energy_output(self, k=160):
         return self.height * self.windspeedaverage * (1 - self.corrosionfactor) * k
+
+    def calculate_resource_depletion_rate(self, k=1200):
+        if self.height  <= 0 or self.windspeedaverage <= 0 or (1 - self.corrosionfactor) <= 0:
+            raise ResourceDepletionRateCalculateError
+        return k / (self.height * self.windspeedaverage * (1 - self.corrosionfactor))
--- tests.py
+++ tests.py
@@ -5,23 +5,39 @@ class EnergySourceTest(unittest.TestCase):
 
     def test_solarpanel(self):
         solarpanel = energy_source.SolarPanel(area=20, efficiency=15)
-        expected_result = 4500
-        self.assertEqual(solarpanel.calculate_result(), expected_result)
+
+        annual_energy_output_expected_result = 4500
+        self.assertEqual(solarpanel.calculate_annual_energy_output(), annual_energy_output_expected_result)
+
+        resource_depletion_rate_expected_result = 6.67
+        self.assertEqual(round(solarpanel.calculate_resource_depletion_rate(), 2), resource_depletion_rate_expected_result)
 
     def test_windturbine(self):
         windturbine = energy_source.WindTurbine(height=50, windspeedaverage=6)
-        expected_result = 45000
-        self.assertEqual(windturbine.calculate_result(), expected_result)
+
+        annual_energy_output_expected_result = 45000
+        self.assertEqual(windturbine.calculate_annual_energy_output(), annual_energy_output_expected_result)
+
+        resource_depletion_rate_expected_result = 3.33
+        self.assertEqual(round(windturbine.calculate_resource_depletion_rate(), 2), resource_depletion_rate_expected_result)
 
     def test_hydroplant(self):
         hydroplant = energy_source.HydroPlant(flowrate=300, drop=20)
-        expected_result = 72000
-        self.assertEqual(hydroplant.calculate_result(), expected_result)
+
+        annual_energy_output_expected_result = 72000
+        self.assertEqual(hydroplant.calculate_annual_energy_output(), annual_energy_output_expected_result)
+
+        resource_depletion_rate_expected_result = 15
+        self.assertEqual(round(hydroplant.calculate_resource_depletion_rate(), 2), resource_depletion_rate_expected_result)
 
     def test_offshorewindturbine(self):
         offshorewindturbine = energy_source.OffshoreWindTurbine(height=70, windspeedaverage=8, corrosionfactor=0.2)
-        expected_result = 71680
-        self.assertEqual(offshorewindturbine.calculate_result(), expected_result)
+
+        annual_energy_output_expected_result = 71680
+        self.assertEqual(offshorewindturbine.calculate_annual_energy_output(), annual_energy_output_expected_result)
+
+        resource_depletion_rate_expected_result = 2.68
+        self.assertEqual(round(offshorewindturbine.calculate_resource_depletion_rate(), 2), resource_depletion_rate_expected_result)
 
     def test_invalid_data_types(self):
         with self.assertRaises(TypeError):
